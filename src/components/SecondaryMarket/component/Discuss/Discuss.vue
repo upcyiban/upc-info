9518482044cd4b6c4444cc5fe5ae7afe1504b2f6
1409070209 <1142908626@qq.com>
2018-04-15 19:54:15 +0800

    '二手市场'

---
+++ src/components/SecondaryMarket/component/Discuss/Discuss.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="Discuss">
+    </div>
+</template>
+
+<script>
+    const Discuss = {
+        name: 'Discuss',
+        data () {
+            return {}
+        }
+    }
+    export default Discuss
+</script>
+
+
+<style scoped>
+    .Discuss {
+
+    }
+</style>
