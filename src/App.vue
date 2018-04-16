4890d2b5ec32d23ff6140cd9f1044e9645384e21
Semesse <tungo@semesse.me>
2018-04-16 17:46:53 +0800

    change app.vue to display changes on My

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
     <div id="app">
         <router-view/>
         <discuss></discuss>
+        <my></my>
     </div>
 </template>
 
 <script>
+    import My from '@/components/SecondaryMarket/component/My/My'
     import Discuss from './components/SecondaryMarket/component/Discuss/Discuss.vue'
     export default {
         name: 'app',
         components: {
-            Discuss
+            'Discuss': Discuss,
+            'My': My
         }
     }
 </script>
